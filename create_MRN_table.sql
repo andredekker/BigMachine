6dcc61a937d75c2358b80b28e9a3432ae22346ae
unknown <andre.dekker@L-11816.ad.maastro.nl>
2016-08-09 17:02:15 -0400

    Initial work on feasibility of issues. The create_mrn_table.sql and generate_mapping_mrn.ttl create table and are a D2RQ mapping to create an RDF version of the MRNs.

---
+++ create_MRN_table.sql
@@ -0,0 +1,27 @@
+﻿-- Query for PostgreSQL to generate a table with 100 sample MRNs for a 7 digit MRN with leading zeros
+-- Andre Dekker, 09-08-2016
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Table: "MRN"
+
+DROP TABLE "MRN";
+
+CREATE TABLE "MRN"
+(
+  "MRN" character(7)
+  
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "MRN"
+  OWNER TO postgres;
+
+-- Insert MRNs, Tokens are created as UUID
+INSERT INTO "MRN"("MRN")
+    SELECT (to_char(generate_series(100, 200),'FM0000000'));
+
+-- Check
+SELECT * FROM "MRN" LIMIT 10
+
